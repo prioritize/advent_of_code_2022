f3b9d78d43968f71aca35e2779ff47cfcaf04edc
prioritize <leach.jared@gmail.com>
2022-12-06 16:32:37 -0800

    formatting day_4

--- src/days/day_3.rs
+++ src/days/day_3.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
-use std::io::Read;
 use std::fs::File;
+use std::io::Read;
 use std::iter::zip;
 
 fn make_priority_map() -> HashMap<char, u32> {
-    let mut  hm = HashMap::new();
+    let mut hm = HashMap::new();
     let alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let mut increment = 1;
     alpha.chars().for_each(|x| {
@@ -14,99 +14,105 @@ fn make_priority_map() -> HashMap<char, u32> {
     hm
 }
 struct DayThree {
-    out: u32
+    out: u32,
 }
 impl DayThree {
     fn parse(filename: String) {
         let mut file = File::open(&filename).expect(&format!("Unable to open {}", filename));
         let mut f_string = String::new();
         file.read_to_string(&mut f_string);
-        let duplicates: Vec<char> = f_string.lines().map(|x| {
-            let center = x.len()/2;
-            let strang = x.to_string();
-            let begin = strang[0..center].to_string();
-            let end = strang[center..].to_string();
-            find_char(begin, end)
-        }).collect();
+        let duplicates: Vec<char> = f_string
+            .lines()
+            .map(|x| {
+                let center = x.len() / 2;
+                let strang = x.to_string();
+                let begin = strang[0..center].to_string();
+                let end = strang[center..].to_string();
+                find_char(begin, end)
+            })
+            .collect();
         let p = make_priority_map();
-        let priority_sum = duplicates.iter().map(|x| {
-            p.get(x).unwrap()
-        }).fold(0, |acc, v| {
-            acc + v
-        });
+        let priority_sum = duplicates
+            .iter()
+            .map(|x| p.get(x).unwrap())
+            .fold(0, |acc, v| acc + v);
         println!("The sum of the priorities is {}", priority_sum);
-        let maps: Vec<HashMap::<char, u32>> = f_string.lines().map(|x| {
-            let mut hm = HashMap::new();
-            x.chars().for_each(|v| {
-                match hm.get(&v) {
-                    None => {hm.insert(v, 1);},
+        let maps: Vec<HashMap<char, u32>> = f_string
+            .lines()
+            .map(|x| {
+                let mut hm = HashMap::new();
+                x.chars().for_each(|v| match hm.get(&v) {
+                    None => {
+                        hm.insert(v, 1);
+                    }
                     Some(count) => {
                         hm.insert(v, count + 1);
                     }
-                }
-            });
-            hm
-        }).collect();
-        let groups: Vec<&[HashMap::<char, u32>]> = maps.chunks(3).collect();
-        let chars: Vec<char> = groups.iter().map(|x| {
-           find_shared_character(&x[0], &x[1], &x[2])
-        }).collect();
-        let badge_total = chars.iter().map(|x| {
-            p.get(x).unwrap()
-        }).fold(0, |acc, v| {acc + v});
+                });
+                hm
+            })
+            .collect();
+        let groups: Vec<&[HashMap<char, u32>]> = maps.chunks(3).collect();
+        let chars: Vec<char> = groups
+            .iter()
+            .map(|x| find_shared_character(&x[0], &x[1], &x[2]))
+            .collect();
+        let badge_total = chars
+            .iter()
+            .map(|x| p.get(x).unwrap())
+            .fold(0, |acc, v| acc + v);
         println!("the priority total of the badges is {}", badge_total);
     }
-
 }
-fn find_char(begin: String, end:String) -> char {
-    let mut hm: HashMap<char, u32>= HashMap::new();
+fn find_char(begin: String, end: String) -> char {
+    let mut hm: HashMap<char, u32> = HashMap::new();
     let mut out: char = '9';
 
     for (l, r) in begin.chars().zip(end.chars()) {
         match hm.insert(l, 0) {
-            None => {},
+            None => {}
             Some(v) => {
                 if v != 0 {
-                    return l
+                    return l;
                 }
             }
         }
         match hm.insert(r, 1) {
-            None => {},
+            None => {}
             Some(v) => {
                 if v != 1 {
-                    return r
+                    return r;
                 }
             }
         };
-    };
+    }
     '!'
 }
-fn find_shared_character(first: &HashMap::<char, u32>, second: &HashMap::<char, u32>, third: &HashMap::<char, u32>) -> char {
-    let answer: Vec<Option<&char>> = first.iter().map(|(k, v)| {
-        match second.get(k) {
-            Some(_) => {
-                 match third.get(k) {
-                     Some(_) => {
-                         Some(k)
-                     }
-                     None => {None}
-                 }
+fn find_shared_character(
+    first: &HashMap<char, u32>,
+    second: &HashMap<char, u32>,
+    third: &HashMap<char, u32>,
+) -> char {
+    let answer: Vec<Option<&char>> = first
+        .iter()
+        .map(|(k, v)| match second.get(k) {
+            Some(_) => match third.get(k) {
+                Some(_) => Some(k),
+                None => None,
             },
-            None => {None}
-        }
-    }).filter(|x| {
-        match x {
+            None => None,
+        })
+        .filter(|x| match x {
             None => false,
-            Some(_) => true
-        }
-    }).collect();
+            Some(_) => true,
+        })
+        .collect();
     answer[0].unwrap().clone()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::days::day_3::{DayThree, make_priority_map};
+    use crate::days::day_3::{make_priority_map, DayThree};
 
     #[test]
     fn parse_day_three() {
---
+++ src/days/day_5.rs
@@ -0,0 +1,15 @@
+use std::fs::File;
+use std::io::Read;
+
+struct DayFive {
+    out: String,
+}
+
+impl DayFive {
+    fn parse(filename: String) {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
