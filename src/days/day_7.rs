c43bcd9c72fb2da1c79e4d074e53bb1cbd3070d3
prioritize <leach.jared@gmail.com>
2022-12-10 08:41:09 -0800

    Commit prior to major refactor

--- src/days/day_7.rs
+++ src/days/day_7.rs
@@ -4,117 +4,92 @@ use std::fs::File;
 use std::io::Read;
 use regex::{Captures, Match, Regex};
 
-pub trait AoC: Display {
-    fn print(&self){
-        println!("This is super functional")
-    }
+struct Directory {
+    files: HashMap<String, u32>,
+    directories: HashMap<String, Directory>,
+    parent: Option<String>,
+    name: String,
+    size_contained: u32,
 }
-enum AoCObject{
-    File {
-        name: String,
-        size: u32,
-    },
-    Directory {
-        files: HashMap::<String, AoCObject>,
-        dirs: HashMap::<String, AoCObject>,
-        name: String,
-        parent: Option<String>,
-    },
-    LS,
-    CD {
-        next_dir: String,
+impl Directory {
+    fn add_dir(&mut self, name: String) {
+       match self.directories.get(&name) {
+           None => {
+               self.directories.insert(name.clone(), Directory::new(name.clone()));
+           }
+           Some(_) => {
+               println!("There was already a directory named {} in the directory named {}", name, self.name);
+           }
+       };
     }
-
-}
-impl AoC for AoCObject {}
-impl Display for AoCObject {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match &self {
-            AoCObject::File{ name, size } => {
-                write!(f, "AoCFile( Name: {}, Size: {})", name, size)
-            }
-            AoCObject::Directory { name,  .. } => {
-                write!(f, "AoCDir( Name: {})", name)
-            }
-            AoCObject::LS => {
-                write!(f, "LS")
-            }
-            AoCObject::CD { next_dir} => {
-                write!(f, "CD ( next: {} )", next_dir)
-            }
-        }
-    }
-}
-impl AoCObject {
-    pub fn new_file(name: String, size: u32) -> Self {
-        AoCObject::File{
-            name,
-            size
-        }
-    }
-    pub fn new_dir(name: String, parent: Option<String>) -> Self {
-        AoCObject::Directory {
+    fn new(name: String) -> Self {
+        Directory {
             files: HashMap::new(),
-            dirs: HashMap::new(),
+            directories: HashMap::new(),
+            parent: None,
             name,
-            parent,
+            size_contained: 0
         }
     }
-    fn new_cd(dest: String) -> Self {
-        AoCObject::CD { next_dir: dest }
+    fn change_directory(&mut self, name: String) -> &mut Directory {
+        self.directories.get(&name).unwrap()
     }
-    fn new_ls () -> Self {
-        AoCObject::LS
+    fn add_file(&mut self, name: String, size: u32) {
+        match self.files.get(&name) {
+            None => {
+                self.files.insert(name.clone(), size);
+                self.size_contained = self.size_contained + size;
+            }
+            Some(_) => {
+                println!("There was already a file named {} in the directory named {}", name, self.name);
+            }
+        };
     }
 }
 struct DaySeven {
-    out: AoCObject
+    out: Directory
 }
 impl DaySeven {
     fn parse(filename: String) {
         let mut file = File::open(&filename).expect(&format!("Unable to open {}", filename));
         let mut f_string = String::new();
-        file.read_to_string(&mut f_string)
-            .expect("unable to parse the file to a string");
-        let re = Regex::new(r"(\$ (?P<cmd>cd|ls) ?(?P<dest_dir>[\w\\/\.]{1,20})?)|(?P<object>((?P<folder>dir) (?P<name>\w{1,20}))|(?P<fsize>\d{1,20}) (?P<fname>[\w.]{1,20}))").unwrap();
-        let mut lines: Vec<AoCObject> = Vec::new();
-        for cap in re.captures_iter(&f_string) {
-            match cap.name("cmd"){
-                None => {},
-                Some(cmd) => {
-                    match cmd.as_str() {
-                        "ls" => {
-                            lines.push(AoCObject::new_ls());
-                            continue;
-                        }
-                        "cd" => {
-                            lines.push(AoCObject::new_cd(cap.name("dest_dir").unwrap().as_str().to_string()));
-                            continue;
-                        }
-                        _ => {}
+        let mut head = Directory::new("/".to_string());
+        let mut current = &mut head;
+        file.read_to_string(&mut f_string).expect(&format!("Unable to open {}", &filename));
+        let re = Regex::new(r"/(\$ cd ([\w\/.]*)\n)|(\$ ls\n)|(dir ([\w.]{1,20})\n)|(([\d]{1,20}) ([\w.]{1,20})\n)").unwrap();
+        let mut iter = f_string.lines();
+        while let Some(line) = iter.next() {
+            let cap = re.captures(line).unwrap();
+            match cap.get(2) {
+                None => {}
+                Some(v) => {
+                    if v.as_str().to_string() == "/".to_string() {
+                        current = &mut head;
+                    } else {
+                        current = current.change_directory(v.as_str().to_string())
                     }
                 }
             }
-            match cap.name("folder") {
+            match cap.get(3) {
+            //     We need to iterate here until we get to another  cd or ls
                 None => {},
                 Some(_) => {
-                    lines.push(AoCObject::new_dir(cap.name("name").unwrap().as_str().to_string(), None));
-                    continue
+                    while let Some(entry) = iter.next() {
+                        let line_cap = re.captures(entry).unwrap();
+                        match line_cap.get(5) {
+                            None => {},
+                            Some(dir) => {
+                                match current.directories.get(dir.as_str()) {
+                                    None => {current.add_dir(dir.as_str().to_string())}
+                                    Some(_) => {}
+                                }
+                            }
+                        }
+                    }
                 }
-            }
-            match cap.name("fname") {
-                None => {},
-                Some(n) => {
-                    lines.push(AoCObject::new_file(n.as_str().to_string(), cap.name("fsize").unwrap().as_str().parse::<u32>().unwrap()));
-                    continue
-                }
-            }
 
+            }
         }
-        for line in lines {
-            println!("{}", line);
-        }
-
     }
 }
 
@@ -124,7 +99,6 @@ mod tests {
 
     #[test]
     fn test_day_7() {
-        let dir = AoCObject::new_dir("/".to_string(), None);
         DaySeven::parse("input/day_7_input.txt".to_string());
         assert_eq!(0, 0)
     }
