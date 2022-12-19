c836d04efdb2a7a5cec5d0a4e4a48e2e8bcbf356
Jared <leach.jared@gmail.com>
2022-12-19 00:53:35 -0800

    Finished day 8

--- src/days/day_8.rs
+++ src/days/day_8.rs
@@ -1,7 +1,113 @@
 use itertools::enumerate;
 use std::fs::File;
 use std::io::Read;
+struct Tree {
+    height: u32,
+    row: usize,
+    col: usize,
+}
+struct Forest {
+    trees: Vec<Vec<Tree>>,
+}
 
+impl Tree {
+    pub fn look(
+        location: (usize, usize),
+        height: u32,
+        direction: &Direction,
+        forest: &Forest,
+    ) -> Option<bool> {
+        match forest.next((location.0, location.1), direction) {
+            None => None,
+            Some(tree) => Some(height > tree.height),
+        }
+    }
+    fn measure(&self, direction: &Direction, forest: &Forest) -> u32 {
+        let mut loc = (self.row, self.col);
+        let mut acc = 0;
+        loop {
+            match Self::look(loc, self.height, direction, forest) {
+                None => break,
+                Some(v) => match v {
+                    true => {
+                        acc = acc + 1;
+                        loc = neighbor(loc, direction);
+                    }
+                    false => {
+                        acc = acc + 1;
+                        break;
+                    }
+                },
+            }
+        }
+        acc
+    }
+}
+
+impl Forest {
+    fn new(filename: String) -> Self {
+        let mut file = File::open(&filename).unwrap();
+        let mut string_buf = String::new();
+        let f_string = file.read_to_string(&mut string_buf).unwrap();
+        let mut trees: Vec<Vec<Tree>> = Vec::new();
+        for (r, line) in enumerate(string_buf.lines()) {
+            let mut col = 0;
+            trees.push(
+                line.chars()
+                    .map(|x| {
+                        let out = Tree {
+                            height: x.to_digit(10).unwrap(),
+                            row: r,
+                            col,
+                        };
+                        col = col + 1;
+                        out
+                    })
+                    .collect(),
+            )
+        }
+        Forest { trees }
+    }
+    fn next(&self, current: (usize, usize), direction: &Direction) -> Option<&Tree> {
+        let neighbor = neighbor(current, &direction);
+        return match self.trees.get(neighbor.0) {
+            None => None,
+            Some(v) => v.get(neighbor.1),
+        };
+    }
+    fn best_view(&self) -> u32 {
+        let views: Vec<Vec<u32>> = self
+            .trees
+            .iter()
+            .map(|r| {
+                r.iter()
+                    .map(|c| {
+                        c.measure(&Direction::North, &self)
+                            * c.measure(&Direction::South, &self)
+                            * c.measure(&Direction::East, &self)
+                            * c.measure(&Direction::West, &self)
+                    })
+                    .collect()
+            })
+            .collect();
+        *views
+            .iter()
+            .map(|v| *v.iter().max().unwrap())
+            .collect::<Vec<u32>>()
+            .iter()
+            .max()
+            .unwrap()
+        // *maxes.iter().max().unwrap()
+    }
+}
+fn neighbor(loc: (usize, usize), direction: &Direction) -> (usize, usize) {
+    match direction {
+        Direction::North => (loc.0 - 1, loc.1),
+        Direction::South => (loc.0 + 1, loc.1),
+        Direction::East => (loc.0, loc.1 + 1),
+        Direction::West => (loc.0, loc.1 - 1),
+    }
+}
 struct DayEight {}
 impl DayEight {
     fn open_input_file(filename: String) -> Vec<Vec<u32>> {
@@ -9,6 +115,23 @@ impl DayEight {
         let mut string_buf = String::new();
         let f_string = file.read_to_string(&mut string_buf).unwrap();
         // let mut tree_height = Vec::new();
+        let mut trees: Vec<Vec<Tree>> = Vec::new();
+        for (r, line) in enumerate(string_buf.lines()) {
+            let mut col = 0;
+            trees.push(
+                line.chars()
+                    .map(|x| {
+                        let out = Tree {
+                            height: x.to_digit(10).unwrap(),
+                            row: r,
+                            col,
+                        };
+                        col = col + 1;
+                        out
+                    })
+                    .collect(),
+            )
+        }
         let tree_height = string_buf
             .lines()
             .map(|x| x.chars().map(|v| v.to_digit(10).unwrap()).collect())
@@ -20,8 +143,9 @@ enum Direction {
     North,
     South,
     East,
-    West
+    West,
 }
+
 fn can_see_tree(height: &u32, highest: &u32) -> bool {
     height > highest
 }
@@ -93,7 +217,6 @@ fn vertical(trees: &Vec<Vec<u32>>, visible: &mut Vec<Vec<bool>>) {
         }
     }
 }
-fn from_down() {}
 
 #[cfg(test)]
 mod tests {
@@ -138,5 +261,15 @@ mod tests {
             .collect();
         let out = visible_count.iter().fold(0, |acc, v| acc + v);
         println!("Total visible trees: {}", out);
+    }
+    #[test]
+    fn part_two() {
+        let forest = Forest::new("input/day_8_input.txt".to_string());
+        println!("{}", forest.best_view());
+    }
+    #[test]
+    fn part_two_test() {
+        let forest = Forest::new("input/day_8_test_input.txt".to_string());
+        println!("{}", forest.best_view());
     }
 }
